180d38431d7e3193783cbf3957e5dc21a1d1d49c
Brandon Bey <brandonbey@gmail.com>
2017-07-27 12:15:24 -0700

    Added stageWidth stageHeight accessors to Lib

--- src/newp/Lib.hx
+++ src/newp/Lib.hx
@@ -16,12 +16,15 @@ class Lib {
 
   public static var main (get, never) :Sprite;
   public static var stage (get, never) :Stage;
+  public static var stageHeight (get, never) :Int;
+  public static var stageWidth (get, never) :Int;
   public static var sceneLayer (get, never) :Sprite;
   public static var debugLayer (get, never) :Sprite;
   public static var scenes (get, never) :SceneManager;
   public static var inputs (get, never) :InputManager;
   public static var delta (get, never) :Float;
   public static var rawDelta (get, never) :Float;
+  public static var time (get, never) :Float;
 
   // Properties
   static inline function get_debug() :Bool { return Lib.engine != null ? Lib.engine.debug : false; }
@@ -31,12 +34,18 @@ class Lib {
   static inline function set_gamespeed(val:Float) :Float { return Lib.engine != null ? Lib.engine.gamespeed = val : 1; }
 
   static inline function get_main () :Sprite { return Lib.engine != null ? Lib.engine.main : null; }
+  
   static inline function get_stage () :Stage { return Lib.engine != null ? Lib.engine.stage : null; }
+
+  static inline function get_stageWidth () :Int { return Lib.engine != null ? Lib.engine.stage.stageWidth : 0; }
+  static inline function get_stageHeight () :Int { return Lib.engine != null ? Lib.engine.stage.stageHeight : 0; }
+
   static inline function get_sceneLayer () :Sprite { return Lib.engine != null ? Lib.engine.sceneLayer : null; }
   static inline function get_debugLayer () :Sprite { return Lib.engine != null ? Lib.engine.debugLayer : null; }
   static inline function get_scenes () :SceneManager { return Lib.engine != null ? Lib.engine.scenes : null; }
   static inline function get_inputs () :InputManager { return Lib.engine != null ? Lib.engine.inputs : null; }
   static inline function get_delta() :Float { return Lib.engine != null ? Lib.engine.clock.delta * Lib.gamespeed : 0; }
   static inline function get_rawDelta() :Float { return Lib.engine != null ? Lib.engine.clock.delta : 0; }
+  static inline function get_time() :Float { return Lib.engine != null ? Lib.engine.clock.now : 0; }
 
 }
